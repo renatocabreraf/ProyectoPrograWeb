85cea0a5ce2544551a85237402db83bec5e1036c
Renato <renatojose98@yahoo.com>
2021-04-21 12:20:59 -0600

    Update hotwheels.controller.js

--- app/app/controllers/hotwheels.controller.js
+++ app/app/controllers/hotwheels.controller.js
@@ -32,12 +32,18 @@ exports.create = function(req, res){
     var newCollector = req.body;
     collectors["hotwheels" + newCollector.id] = newCollector;
     console.log("--->After Post, collectors: \n" + JSON.stringify(collectors, null, 4));
-    res.end("Post Succesfully: \n" + JSON.stringify(newCollector, null, 4));
+    res.end("Code 201: Post Succesfully: \n" + JSON.stringify(newCollector, null, 4));
 };
 
 exports.findAll = function(req, res){
-    console.log("--->Find All: \n" + JSON.stringify(collectors, null, 4));
-    res.end("All Collectors: \n" + JSON.stringify(collectors, null, 4));
+    if(collectors["hotwheels" + id] !=null){
+        console.log("--->Find All: \n" + JSON.stringify(collectors, null, 4));
+        res.end("All Collectors: \n" + JSON.stringify(collectors, null, 4));
+    } else if(collectors["hotwheels" + id] ==null){
+        res.end("Code 200: Dont Exist Collector ID: \n" + JSON.stringify(updatedCollector, null, 4));
+    } else{
+        res.end("Code 404: Collector didnt Exist: \n" + JSON.stringify(updatedCollector, null, 4));
+    }
 };
 
 exports.findOne = function(req, res){
@@ -52,15 +58,19 @@ exports.update = function(req, res){
     if(collectors["hotwheels" + id] !=null){
         collectors["hotwheels" + id] = updatedCollector;
         console.log("--->Update Successfully, collectors: \n" + JSON.stringify(hotwheels, null, 4));
-        res.end("Update Successfully! \n" + JSON.stringify(updatedCollector, null, 4));
+        res.end("Code 204: Update Successfully! \n" + JSON.stringify(updatedCollector, null, 4));
     }else{
-        res.end("Dont Exist Collector: \n" + JSON.stringify(updatedCollector, null, 4));
+        res.end("Code 404: Collector didnt Exist: \n" + JSON.stringify(updatedCollector, null, 4));
     }
 };
 
 exports.delete = function (req, res){
     var deleteCollector = hotwheels["collector" + req.params.id];
+    if(collectors["hotwheels" + id] !=null){
     delete collectors["hotwheels" + req.params.id];
     console.log("--->After delete, hotwheels list: \n" + JSON.stringify(hotwheels, null, 4));
-    res.end("Delete Successfully! \n" + JSON.stringify(deleteCollector, null, 4));
+    res.end("Code 204: Delete Successfully! \n" + JSON.stringify(deleteCollector, null, 4));
+    }else{
+        res.end("Code 404: Collector didnt Exist: \n" + JSON.stringify(updatedCollector, null, 4));
+    }
 };
